92ff68dd2925ecb53af68d84d1e2edd7e5934e68
mel418 <melodygatan@gmail.com>
2024-05-19 00:16:38 -0700

    heroes and footer

--- app/(marketing)/_components/heroes.tsx
+++ app/(marketing)/_components/heroes.tsx
@@ -2,8 +2,25 @@ import Image from "next/image";
 
 export const Heroes = () => {
     return(
-        <div>
-            Heroes
+        <div className="flex flex-col items-center justify-center max-w-5xl">
+            <div className="flex items-center">
+                <div className="relative w-[300px] h-[300px] sm:w-[350px]sm:h-[350px] md:h-[400px] md:w-[400px]">
+                    <Image
+                        src="/Fast-Internet.svg"
+                        fill
+                        className="object-contain"
+                        alt="Fast Internet"
+                    />
+                </div>
+                <div className="relative h-[400px] w-[400px] hidden md:block">
+                    <Image
+                        src="/Chill-Time.svg"
+                        fill
+                        className="object-contain"
+                        alt="Chill Time"
+                    />
+                </div>
+            </div>
         </div>
     )
 }
