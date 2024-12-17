21fd74e5cca43358e068cc326d11a936c18b8795
mel418 <melodygatan@gmail.com>
2024-12-16 20:07:15 -0800

    most recent commit

---
+++ components/search-command.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { File } from "lucide-react";
+import { useQuery } from "convex/react";
+import { useRouter } from "next/navigation";
+import { useUser } from "@clerk/clerk-react";
+
+import { 
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList
+} from "./ui/command";
+import { useSearch } from "@/hooks/use-search";
+import { api } from "@/convex/_generated/api";
+
+export const SearchCommand = () => {
+  const { user } = useUser();
+  const router = useRouter();
+  const documents = useQuery(api.documents.getSearch);
+}
